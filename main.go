07831080a5297cd9897897fc21bc441ab0565163
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-01-18 15:55:18 +0100

    go style

--- main.go
+++ main.go
@@ -1,36 +1,27 @@
 package main
 
 import (
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"net/http"
 )
 
-/*
-var dictionary Dictionary
-
-func listWords(writer http.ResponseWriter, request *http.Request) {
-  payload, err := json.Marshal(dictionary)
-  if  err != nil {
-    http.Error(writer, err.Error() , http.StatusInternalServerError)
-    return
-  }
-  writer.Write(payload)
+func listWords(writer http.ResponseWriter, request *http.Request, dictionary Dictionary) {
+	payload, err := json.Marshal(dictionary)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.Write(payload)
 }
-*/
 
 func main() {
-  dictionary := Dictionary{Origin: "itsme"}
-  dictionary.add("foo","is not bar")
-  dictionary.add("bar","is not foo")
-  dictionary.add("qix","is not foo nor bar")
+	dictionary := Dictionary{Origin: "itsme"}
+	dictionary.add("foo", "is not bar")
+	dictionary.add("bar", "is not foo")
+	dictionary.add("qix", "is not foo nor bar")
 
-  http.HandleFunc("/list", func (writer http.ResponseWriter, request *http.Request) {
-    payload, err := json.Marshal(dictionary)
-    if  err != nil {
-      http.Error(writer, err.Error() , http.StatusInternalServerError)
-      return
-    }
-    writer.Write(payload)
-  })
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/list", func(writer http.ResponseWriter, request *http.Request) {
+		listWords(writer, request, dictionary)
+	})
+	http.ListenAndServe(":8080", nil)
 }
