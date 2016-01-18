8b2dfc89c7993f777e522e9e8c9694fe0d3e554d
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-01-18 16:58:03 +0100

    Read/Write definition

--- dictionary.go
+++ dictionary.go
@@ -1,16 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"sync"
+)
+
 type Definition struct {
-  Origin string
-  Word string
-  Explanation string
+	Origin      string
+	Word        string
+	Explanation string
+}
+
+func newDictionary(origin string) Dictionary {
+	return Dictionary{Origin: origin, locker: &sync.Mutex{}}
 }
 
 type Dictionary struct {
-  Origin string
-  Definitions []Definition
+	Origin      string
+	Definitions []Definition
+	locker      sync.Locker
 }
 
 func (d *Dictionary) add(word, explanation string) {
-  d.Definitions = append(d.Definitions, Definition{word, explanation, d.Origin})
+	d.locker.Lock()
+	defer d.locker.Unlock()
+	d.Definitions = append(d.Definitions, Definition{word, explanation, d.Origin})
+}
+
+func (d *Dictionary) addDefinition(definition Definition) {
+	d.locker.Lock()
+	defer d.locker.Unlock()
+	d.Definitions = append(d.Definitions, definition)
+}
+
+func (d *Dictionary) toJson() ([]byte, error) {
+	d.locker.Lock()
+	defer d.locker.Unlock()
+	return json.Marshal(d)
 }
--- main.go
+++ main.go
@@ -3,10 +3,37 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gorilla/mux"
 )
 
-func listWords(writer http.ResponseWriter, request *http.Request, dictionary Dictionary) {
-	payload, err := json.Marshal(dictionary)
+func putWord(writer http.ResponseWriter, request *http.Request, dictionary *Dictionary) {
+	definition, err := readDefinition(request)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+	dictionary.addDefinition(definition)
+	writer.WriteHeader(http.StatusCreated)
+}
+
+func readDefinition(request *http.Request) (Definition, error) {
+	bytes, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return Definition{}, err
+	}
+	definition := &Definition{}
+	err = json.Unmarshal(bytes, definition)
+	if err != nil {
+		return Definition{}, err
+	}
+	return *definition, nil
+}
+
+func listWords(writer http.ResponseWriter, request *http.Request, dictionary *Dictionary) {
+	payload, err := dictionary.toJson()
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
@@ -14,14 +41,24 @@ func listWords(writer http.ResponseWriter, request *http.Request, dictionary Dic
 	writer.Write(payload)
 }
 
+func withDictionary(dictionary *Dictionary, handler func(writer http.ResponseWriter, request *http.Request, dictionary *Dictionary)) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		handler(writer, request, dictionary)
+	}
+}
+
 func main() {
-	dictionary := Dictionary{Origin: "itsme"}
+	router := mux.NewRouter()
+
+	dictionary := newDictionary("itsme")
 	dictionary.add("foo", "is not bar")
 	dictionary.add("bar", "is not foo")
 	dictionary.add("qix", "is not foo nor bar")
 
-	http.HandleFunc("/list", func(writer http.ResponseWriter, request *http.Request) {
-		listWords(writer, request, dictionary)
-	})
-	http.ListenAndServe(":8080", nil)
+	router.HandleFunc("/", withDictionary(&dictionary, listWords)).Methods("GET")
+	router.HandleFunc("/words", withDictionary(&dictionary, listWords)).Methods("GET")
+	router.HandleFunc("/words", withDictionary(&dictionary, putWord)).Methods("POST")
+
+	fmt.Println("Listening...")
+	http.ListenAndServe(":8080", router)
 }
